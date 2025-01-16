618e9770f91f7159d91194ad6fee15cffff833a3
stevey144 <stephenogechi455@gmail.com>
2025-01-16 14:02:47 +0000

    running migrations for database objects

--- mind_haven_web_app/models.py
+++ mind_haven_web_app/models.py
@@ -1,3 +1,56 @@
 from django.db import models
-
 # Create your models here.
+class Contacts(models.Model):
+    GENDER_CHOICES = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ]
+    
+    DISABILITY_CHOICES = [
+        ('yes', 'Yes, I have a disability, or have had one in the past'),
+        ('no', 'No, I do not have a disability and have not had one in the past'),
+        ('no_answer', 'I do not want to answer'),
+    ]
+    
+    FirstName=models.CharField(max_length=50)
+    last=models.CharField(max_length=50)
+    phonenumbers=models.IntegerField()
+    address=models.CharField(max_length=100)
+    
+    
+    def __str__(self):
+        return f"{self.FirstName} {self.last} - Phone: {self.phonenumbers}"
+    
+    
+class Appointment(models.Model):
+    Email=models.CharField(max_length=100)
+    Password= models.CharField(max_length=50)
+        
+    def __str__(self):
+      return self.email
+
+
+class Signup(models.Model):
+    Firstname=models.CharField(max_length=100)
+    Lastname= models.CharField(max_length=100)
+    Email=models.CharField(max_length=100, unique=True)
+    Password=models.CharField(max_length=100)
+    
+    def __str__(self):
+      return f"{self.Firstname} {self.Lastname} {self.Email}"
+ 
+class Booking(models.Model):
+    date = models.DateField()
+    textarea=models.TextField()
+    
+    def __str__(self):
+      return f"{self.date} {self.textarea}"
+    
+    
+    
+
+
+    
+    
+    
+    
--- mindhaven/settings.py
+++ mindhaven/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mind_haven_web_app'
+    'mind_haven_web_app',
 ]
 
 MIDDLEWARE = [
