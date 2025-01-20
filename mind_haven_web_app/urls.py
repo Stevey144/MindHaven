a3d90ad7312eab038c113f01d61a4d198387307a
stevey144 <stephenogechi455@gmail.com>
2025-01-20 21:50:24 +0000

    adjusted

--- mind_haven_web_app/urls.py
+++ mind_haven_web_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('appointment.html',views.sign_in, name="appointment"),
     path('signup.html', views.signup, name='sign-up'),
     path('booking.html', views.booking, name='booking'),
-    path('admin-sign-up.html', views.admin_sign_up_view, name='admin-sign-up'), 
-    path('admin-sign-in.html', views.admin_sign_in_view, name='admin-sign-in'), 
+    path('admin/admin-sign-up.html', views.admin_sign_up_view, name='admin-sign-up'), 
+    path('admin/admin-sign-in.html', views.admin_sign_in_view, name='admin-sign-in'), 
     path('view-booking.html', views.booking_list, name='view-booking'),  
 ]
